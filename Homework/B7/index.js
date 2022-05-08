ddc7d07db5dc92d38343acc09d8c97f80a5b71c6
Đức Chung Phạm <ducchung6996@gmail.com>
2022-05-08 09:24:07 +0700

    first commit

---
+++ Homework/B7/index.js
@@ -0,0 +1,43 @@
+let bruh = 1 !== 1
+let mahbruh = 1 == "1"
+
+console.log(bruh)
+console.log(mahbruh)
+
+// || && ! typeof /n = xuong dong, /t = vao tab
+
+if ("2 > 1") {
+    console.log("sai roi")
+}
+
+let gogo = 0
+
+if (gogo) {
+    console.log("sua cai loz")
+} else {
+    console.log("lmao")
+}
+
+let A = 20
+let B = 30
+
+if (A > B) {
+    console.log("A > B")
+} else if (A === B) {
+    console.log("A = B cung kieu giu lieu")
+} else {
+    console.log("A = B")
+}
+
+let C = A > B ? "more" : (A == B ? "equal" : "less")
+
+console.log(C)
+
+switch (key) {
+    case value:
+        
+        break;
+
+    default:
+        break;
+}
