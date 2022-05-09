eeaab5187e2824ad893e70a7f190b2f393b5a933
Đức Chung Phạm <ducchung6996@gmail.com>
2022-05-09 20:54:03 +0700

    update hw

--- Homework/B7/HW1/index.js
+++ Homework/B7/HW1/index.js
@@ -1,5 +1,5 @@
 let a = prompt("What is your last and middle name ?");
-let b = prompt("What is your first name?");
+let b = prompt("What is your first name ?");
 alert("Hello" + " " + a + " " + b);
 let c = prompt("How old are you ?")
 alert("My name is" + " " + a + " " + b + " " + " " + "," + "i'm" + " " + c + " " + "years old")
--- Homework/B7/HW4/index.js
+++ Homework/B7/HW4/index.js
@@ -1,5 +1,3 @@
-let a = prompt("What is your last and middle name ?");
-let b = prompt("What is your first name?");
-alert("Hello" + " " + a + " " + b);
-let c = prompt("How old are you ?")
-alert("My name is" + " " + a + " " + b + " " + " " + "," + "i'm" + " " + c + " " + "years old")
+const a = prompt("First number ?");
+const b = prompt("Second number ?");
+alert("a + b = " + a + b);
