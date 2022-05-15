ec7020b55b9db03018dd643df66380f393a917ba
Đức Chung Phạm <ducchung6996@gmail.com>
2022-05-15 17:03:42 +0700

    update hw

---
+++ Other/Test/index.js
@@ -0,0 +1,54 @@
+alert("Muhney spending counter");
+
+
+
+let lmao = () => {
+    let totalMuhney = parseInt(prompt("Total muhney you have in this month ?"));
+    let muhneyForHouse = parseInt(prompt("Muhney for rent your house ?"));
+    let muhneyForFood = parseInt(prompt("Muhney for food ?"));
+    let otherMuhney = parseInt(prompt("Muhney you need to spend for other thing ?"))
+
+    function totalMuhneySpendCounter(a, b, c) {
+        return a + b + c;
+    }
+
+    function muhneyLeft(a, b, c, d) {
+        return a - (b + c + d)
+    }
+
+    let question = parseInt(prompt("What you want to count ?\n1. Total muhney you will spend\n2. Total muhney you have left"))
+    switch (question) {
+        case 1:
+            alert(`Total muhney you will spend: ${totalMuhneySpendCounter(muhneyForHouse, muhneyForFood, otherMuhney)} vnd`)
+            break;
+
+        case 2:
+            alert(`Total muhney you have left: ${muhneyLeft(totalMuhney,muhneyForHouse,muhneyForFood,otherMuhney)} vnd`)
+            break;
+
+        default:
+            alert("Wrong choice")
+            break;
+    }
+}
+
+// while (true) {
+//     lmao();
+//     let repeat = parseInt(prompt("Do you wanna try again ?\n1. Yep\n2. Nah"));
+//     if (repeat != 1) {
+//         alert("Later bitch")
+//         break;
+//     }
+// }
+
+for (;;) {
+    lmao();
+    let repeat = parseInt(prompt("Do you wanna try again ?\n1. Yep\n2. Nah"));
+    if (repeat != 1) {
+        alert("Later bitch")
+        break;
+    }
+}
+
+
+
