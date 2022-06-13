0f00713b707075d8725de634381f152fe4995efd
ducchung6996 <89858986+ducchung6996@users.noreply.github.com>
2022-06-13 21:10:42 +0700

    update

--- Homework/B10/index.js
+++ Homework/B10/index.js
@@ -1,25 +1,20 @@
-const weekdays = ["Sun","Mon","Tues","Wed","Thu","Fri","Sat"];
-const months = ["Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"];
+const weekdays = ["Sun", "Mon", "Tues", "Wed", "Thu", "Fri", "Sat"];
+const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
 
 
 function clock() {
     let dateObj = new Date();
-    let days = document.getElementById("days")
-    let hours = document.getElementById("hours");
-    let month = document.getElementById("month");
-    let minutes = document.getElementById("minutes");
-    let seconds = document.getElementById("seconds");
-
-    weekday.innerText = weekdays[dateObj.getDay()];
-    days.innerText = convertNum(dateObj.getDate());
-    month.innerText = months[dateObj.getMonth()];
-    hours.innerText = convertNum(dateObj.getHours());
-    minutes.innerText = convertNum(dateObj.getMinutes());
-    seconds.innerText = convertNum(dateObj.getSeconds());
+    document.getElementById("weekday").innerText = weekdays[dateObj.getDay()];
+    document.getElementById("days").innerText = convertNum(dateObj.getDate());
+    document.getElementById("month").innerText = months[dateObj.getMonth()];
+    document.getElementById("hours").innerText = convertNum(dateObj.getHours());
+    document.getElementById("minutes").innerText = convertNum(dateObj.getMinutes());
+    document.getElementById("seconds").innerText = convertNum(dateObj.getSeconds());
 }
 
 function convertNum(num) {
-    return  num <  10 ? "0" + num : num;
+    return num < 10 ? "0" + num : num;
 }
+
 let timeConting = setInterval(clock, 1000);
 
--- Homework/B8/HW3/index.js
+++ Homework/B8/HW3/index.js
@@ -1,50 +1,66 @@
-let date = parseInt(prompt('Nhập vào ngày'));
-let month = parseInt(prompt('Nhập vào tháng'));
-let year = parseInt(prompt('Nhập vào năm'));
-let monthsHave31Days = [1, 3, 5, 7, 8, 10, 12];
+function nextDate() {
+    let date = parseInt(prompt('Nhập vào ngày'));
+    let month = parseInt(prompt('Nhập vào tháng'));
+    let year = parseInt(prompt('Nhập vào năm'));
+    let monthsHave31Days = [1, 3, 5, 7, 8, 10, 12];
 
-if (monthsHave31Days.includes (month)) {
-    totalDayOfThisMonth = 31
-} else if (month == 2 && (year % 4 == 0)) {
-    totalDayOfThisMonth = 29
-} else if (month == 2 && (year % 4 != 0)) {
-    totalDayOfThisMonth = 28
-} else {
-    totalDayOfThisMonth = 30
+    if (monthsHave31Days.includes(month)) {
+        totalDayOfThisMonth = 31;
+    } else if (month == 2 && (year % 4 == 0)) {
+        totalDayOfThisMonth = 29;
+    } else if (month == 2 && (year % 4 != 0)) {
+        totalDayOfThisMonth = 28;
+    } else {
+        totalDayOfThisMonth = 30;
+    }
+
+    if (date == totalDayOfThisMonth) {
+        date = 1;
+        month += 1;
+    } else {
+        date += 1;
+    }
+
+    if (month >= 12) {
+        month = 1;
+        year += 1;
+    }
+
+    if (date > 0 && date <= totalDayOfThisMonth) {
+        dateFormat = true;
+    } else {
+        dateFormat = false;
+    }
+
+    if (month > 0 && month <= 12) {
+        monthFormat = true;
+    } else {
+        monthFormat = false;
+    }
+
+    if (year > 0) {
+        yearFormat = true;
+    } else {
+        yearFormat = false;
+    }
+
+    if (dateFormat == true && monthFormat == true && yearFormat == true) {
+        alert(`Ngày mai là:\nNgày ${date} tháng ${month} năm ${year}`);
+    } else {
+        alert(`Bạn đã nhập sai`);
+    }
 }
 
-if (date == totalDayOfThisMonth) {
-    date = 1
-    month += 1
-} else {
-    date += 1
-}
+nextDate();
 
-if (month >= 12) {
-    month = 1
-    year +=1
-}
-
-if (date > 0 && date <= totalDayOfThisMonth) {
-    dateFormat = true
-} else {
-    dateFormat = false
-}
-
-if (month > 0 && month <= 12) {
-    monthFormat = true
-} else {
-    monthFormat = false
-}
-
-if (year > 0) {
-    yearFormat = true
-} else {
-    yearFormat = false
-}
-
-if (dateFormat == true && monthFormat == true && yearFormat == true) {
-    alert(`Ngày mai là:\nNgày ${date} tháng ${month} năm ${year}`)
-} else {
-    alert(`Bạn đã nhập sai`)
+while (true) {
+    const repeat = prompt("Bạn có muốn tiếp tục ? (Y/N)");
+    if (repeat.toUpperCase() == "Y") {
+        nextDate();
+    } else if (repeat.toUpperCase() == "N") {
+        alert("Gud bye");
+        break;
+    } else {
+        alert("Bạn đã nhập sai");
+    }
 }
