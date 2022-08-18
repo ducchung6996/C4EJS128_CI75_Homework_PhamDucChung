054b524863ff14a05b22ce5905a87a28c5568c3b
ducchung6996 <89858986+ducchung6996@users.noreply.github.com>
2022-08-18 16:01:25 +0700

    update

---
+++ WEB61/session1_nodejs_introduction/basic/index.js
@@ -0,0 +1,22 @@
+// const fs = require("fs");
+
+// const content = "hello";
+
+// // fs.writeFile("test.txt", content, (err, data) => {
+// //   if (err) {
+// //     console.log(err);
+// //     return;
+// //   }
+// //   console.log(data);
+// // });
+
+// fs.readFile("test.txt", 'utf-8', (err, data) => {
+//     if (err) {
+//         console.log(err);
+//         return;
+//     }
+//     console.log(data);
+// })
+
+// const math = require('./math');
+// console.log(math.sub(5, 6));
